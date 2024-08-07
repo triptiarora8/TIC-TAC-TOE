3a8a3ffc835c354da8c80bfe0bf350a62ed4d9c4
tripti arora <triptiarora820@gmail.com>
2024-08-07 21:41:10 +0530

    first commit

---
+++ src/components/Player.jsx
@@ -0,0 +1,37 @@
+import React, { useState } from 'react'
+
+export default function Player({ initialName, symbol, isActive}) {
+    const [playerName, setPlayerName] = useState(initialName);
+    const [editName, setEditName] = useState(false);
+
+
+    function handleEdit() {
+        setEditName((editing) => !editing);
+    }
+
+    function handleChangePlayerName(event) {
+        setPlayerName(event.target.value);
+    }
+
+    function handleKeyPress(event) {
+        if (event.key === 'Enter') {
+            setEditName(false);
+        }
+    }
+    let editPlayerName = <span className="player-name">{playerName}</span>
+    // let btnCaption = "Edit";
+
+    if (editName) {
+        editPlayerName = <input type="text" value={playerName} onChange={handleChangePlayerName} onKeyDown={handleKeyPress} ></input>
+        // btnCaption = "Save";
+    }
+    return (
+        <li className={isActive ? 'active': undefined}>
+            <span className="player">
+                {editPlayerName}
+                <span className="player-symbol">{symbol}</span>
+            </span>
+            <button onClick={handleEdit}>{editName ? 'Save' : 'Edit'}</button>
+        </li>
+    )
+}   
